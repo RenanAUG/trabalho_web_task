33dc82dd68c5159d8035a54f0cc93af1a0107f52
lucaspessoli <pessolilucas@gmail.com>
2024-04-13 17:15:08 -0300

    IMPLEMENT TaskService

--- task/src/main/java/br/unipar/programacaointernet/task/task/controller/TaskController.java
+++ task/src/main/java/br/unipar/programacaointernet/task/task/controller/TaskController.java
@@ -1,4 +1,15 @@
 package br.unipar.programacaointernet.task.task.controller;
 
+import br.unipar.programacaointernet.task.task.service.TaskService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/task")
 public class TaskController {
+
+
+
 }
--- task/src/main/java/br/unipar/programacaointernet/task/task/service/TaskService.java
+++ task/src/main/java/br/unipar/programacaointernet/task/task/service/TaskService.java
@@ -1,4 +1,33 @@
 package br.unipar.programacaointernet.task.task.service;
 
+import br.unipar.programacaointernet.task.task.model.Task;
+import br.unipar.programacaointernet.task.task.repository.TaskRepository;
+import jakarta.inject.Inject;
+
+import java.util.List;
+
 public class TaskService {
+
+    @Inject
+    private TaskRepository taskRepository;
+
+    public List<Task> listar() throws Exception{
+        return taskRepository.listarTasks();
+    }
+
+    public void salvar(Task task) throws Exception{
+        taskRepository.cadastrar(task);
+    }
+
+    public void busarPorId(Integer id) throws Exception{
+        taskRepository.buscarPorId(id);
+    }
+
+    public void deletarTask(Integer id) throws Exception{
+        taskRepository.deletar(id);
+    }
+
+    public void editarTask(Task task, Integer id) throws Exception{
+        taskRepository.editarTask(task, id);
+    }
 }
